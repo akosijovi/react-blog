ebd583278b169c2df97e6bbcddf83e08bd8df8bc
akosijovi <jftorrevillas@gmail.com>
2018-09-04 02:17:16 +0800

    Moved some components to containers to be used as pages when routing is implemented.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,53 +1,12 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import { Route } from 'react-router-dom';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostID: null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get('/posts')
-            .then( response => {
-                const posts = response.data.splice(0, 4);
-                const updatedPost = posts.map( post =>{
-                    return {
-                    ...post,
-                    author: 'Jovi'}
-                });
-                this.setState({posts: updatedPost});
-            })
-            .catch( error => {
-                this.setState({error: true})
-            } );
-    }
-
-    selectedPostIDHandler = (id) => {
-        this.setState({selectedPostID: id});
-    }
 
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if(!this.state.error){
-            posts = this.state.posts.map(
-                post => {
-                    return <Post 
-                        key={post.id} 
-                        title={post.title} 
-                        author={post.author}
-                        clicked={() => this.selectedPostIDHandler(post.id)}
-                    />
-                }
-            );
-        }
 
         return (
             <div className="Blog">
@@ -59,17 +18,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost 
-                        id={this.state.selectedPostID}
-                    />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
